3b288d12d9ee3e96236dced85b8716135ec868ca
xavernas <xavernas@gmail.com>
2020-10-04 00:09:58 +0700

    Trial Pertama

--- StringMath.go
+++ StringMath.go
@@ -5,35 +5,68 @@ import (
 	"strconv"
 )
 
-func stringMath(angka string) []int {
-	// Diconvert ke string
-	var slice_string []string
-	for _, value := range angka {
-		slice_string = append(slice_string, string(value))
+// Batasan: Tidak ada kurung
+func stringMath(angka string) int {
+	// Kalau angka biasa yang diinput, tidak perlu diproses
+	if converted_number, err := strconv.Atoi(angka); err == nil {
+		fmt.Println(true)
+		return converted_number
 	}
-	fmt.Print(slice_string)
-	// Kalau tidak ada yang sama, diconvert ke integer
-	var slice_integer []int
-	// var inget_posisi []int
-	for _, nilai := range slice_string {
-		for _, eval := range slice_string {
-			fmt.Print(nilai, eval)
-			if i, err := strconv.Atoi(nilai); err == nil {
-				slice_integer = append(slice_integer, i)
+	index := 0
+	operator := ""
+	angkastring := ""
+	// angka minus tidak akan diproses
+	var hasil_akhir int
+	var indexsementara int
+	for index < len(angka) {
+		indexsementara = index
+		if int(angka[indexsementara]) > 47 && int(angka[indexsementara]) < 58 {
+			for indexsementara < len(angka) && int(angka[indexsementara]) > 47 && int(angka[indexsementara]) < 58 {
+				angkastring += string(angka[indexsementara])
+				indexsementara += 1
+			}
+			// Untuk assign angka pertama
+			if hasil_akhir == 0 && len(string(hasil_akhir)) == 1 {
+				if converted_string, err := strconv.Atoi(angkastring); err == nil {
+					hasil_akhir = converted_string
+				}
+				angkastring = ""
+			}
+		} else if int(angka[index]) == 43 {
+			operator += "+"
+		} else if int(angka[index]) == 45 {
+			operator += "-"
+		}
+		if angkastring != "" && operator != "" {
+			if converted_string, err := strconv.Atoi(angkastring); err == nil {
+				fmt.Println(angkastring, " ", operator)
+				angkaconverted := converted_string
+				if operator == "+" {
+					hasil_akhir += angkaconverted
+				} else if operator == "-" {
+					hasil_akhir -= angkaconverted
+				}
+				angkastring = ""
+				operator = ""
 			}
 		}
+		if indexsementara != index {
+			fmt.Println(index, " ke ", indexsementara)
+			index = indexsementara + 1
+		} else {
+			fmt.Println(index)
+			index += 1
+		}
 	}
-
-	fruit := []int{
-		20,
-		5,
-		5, // comma added
-	}
-	return fruit
+	return hasil_akhir
 }
 func main() {
-	fmt.Println(stringMath("5 + 2 + 1"))            // 8
-	fmt.Println(stringMath("7 - 4 + 21"))           // 24
-	fmt.Println(stringMath("15 + 241 - 741 + -24")) // -509
-	fmt.Println(stringMath("274 - 3561 + 80 - 74")) // -3281
+	// fmt.Println(stringMath("3252"))
+	// fmt.Println()
+	fmt.Println(stringMath("    3427 +      2 "))
+	// fmt.Println()
+	fmt.Println(stringMath("5 + 2 + 1"))  // 8
+	fmt.Println(stringMath("7 - 4 + 21")) // 24
+	// fmt.Println(stringMath("15 + 241 - 741 + -24")) // -509
+	// fmt.Println(stringMath("274 - 3561 + 80 - 74")) // -3281
 }
